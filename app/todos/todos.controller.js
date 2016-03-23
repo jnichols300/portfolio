d321353fed5a2b852e871b24cbef2f60d92baae7
Jared Nichols <jnichols300@gmail.com>
2016-03-23 17:20:54 -0400

    updating directory to convention

--- app/todos/todos.controller.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("todo");
-app.controller("todosController", function(){
-  this.todos = [
-    "Walk the dog",
-    "Buy groceries",
-    "Drink coffee",
-    "Wake up like this"
-  ];
-});
