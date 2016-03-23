04a45454a30ffc088780b8df1436f626c2e3e9d0
Jared Nichols <jnichols300@gmail.com>
2016-03-23 18:19:15 -0400

    setting up routes

--- app/app.js
+++ app/app.js
@@ -1,3 +1,40 @@
+
+
 (function() {
-  angular.module('portfolio', []);
+"use strict";
+  angular
+  .module('portfolio', [
+      "ui-router",
+      "partials"
+    ])
+
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+    function RouterFunction($stateProvider){
+      $stateProvider
+        .state("home", {
+          url: "",
+          template: "The portfolio of Jared Nichols"
+          })
+        .state("development", {
+          url: "/development",
+          templateUrl: "app/partials/development.html",
+          // controller: "",
+        })
+        .state("design", {
+          url: "/design",
+          templateUrl: "app/partials/design.html",
+        })
+        .state("about", {
+          url: "/about",
+          templateUrl: "app/partials/about.html",
+        })
+        .state("contact", {
+          url: "/contact",
+          templateUrl: "app/partials/contact.html",
+        });
+      }
 })();
